21358aa5828dfaced086cd48114e7576d6813e42
BOONEJA22 <booneja22@gcc.edu>
2023-03-01 13:43:28 -0500

    Changed main to ask for main

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Worldy!");
-        System.out.println("Goodbye");
+        System.out.println("What is your name?");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello, " + in.nextLine());
     }
 }
